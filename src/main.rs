036031df7d0b45488a7eb4ee0ae764527f8f1814
alberto tirla <albertotirla@gmail.com>
2021-10-25 11:35:10 +0300

    refactor prototype to use once_cell for access to the speaker object.

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,27 @@
+use std::sync::Mutex;
 use atspi::accessible::AccessibleExt;
-use tts_subsystem::Error;
 use tts_subsystem::Priority;
 use tts_subsystem::Speaker;
+use once_cell::sync::OnceCell;
+fn speak(text:&str) {
+    tts
+        .get()
+        .unwrap()
+        .lock()
+        .unwrap()
+        .speak(Priority::Important, text)
+        .unwrap();
+}
+static tts: OnceCell<Mutex<Speaker>>=OnceCell::new();
 fn main() {
     if let Err(e) = atspi::init() {
         eprintln!("Error initialising libatspi: {}", e);
         std::process::exit(1);
     }
-    let tts=Speaker::new("yggdrasil").unwrap();
+    tts.set(Mutex::new(Speaker::new("yggdrasil").unwrap()));
     let desktop = atspi::desktop(0).expect("Desktop 0 should exist");
     //the following bits of code have a lot of unwrap, will refactor this later
-    tts.speak(Priority::Message, "this program speaks all visible accessible widgets on the screen, including on windows that might not be visible on the screen or to orca").unwrap();
+    speak("this program speaks all visible accessible widgets in the accessibility tree, including on windows that might not be visible on the screen or to orca");
     for (i, child) in desktop
         .children()
         .expect("Could not get number of children of desktop")
@@ -19,6 +30,8 @@ fn main() {
         let child = child.expect("Could not get child of desktop");
         let name = child.name().expect("Could not get name");
         let child_count = child.child_count().expect("Could not get child count");
-        tts.speak(Priority::Important, &name).unwrap();
+        speak(&format!("item number: {}", i));
+        speak(&name);
+        speak(&format!("node number: {}", child_count));
     }
 }
