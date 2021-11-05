7be49ab32a58786780c7ea50be2db7ae532c9526
Michael Connor Buchan <mikey@blindcomputing.org>
2021-11-05 09:12:52 +0000

    Format code

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use std::{sync::Arc, time::Duration};
 use dbus::{
     channel::Channel,
     message::{MatchRule, SignalArgs},
-    nonblock::{Proxy, MethodReply, SyncConnection, stdintf::org_freedesktop_dbus::Properties},
+    nonblock::{stdintf::org_freedesktop_dbus::Properties, MethodReply, Proxy, SyncConnection},
 };
 use futures::stream::StreamExt;
 use once_cell::sync::OnceCell;
@@ -18,7 +18,7 @@ const TIMEOUT: Duration = Duration::from_secs(1);
 // Create a lazily initialised static Speaker
 // The closure is called when `TTS` is first used, and its return value is used to initialise a
 // hidden OnceCell in this static
-static TTS:OnceCell<Mutex<Speaker>>=OnceCell::new();
+static TTS: OnceCell<Mutex<Speaker>> = OnceCell::new();
 //static TTS: Lazy<Mutex<Speaker>> = Lazy::new(|| Mutex::new(Speaker::new("yggdrasil").unwrap()));
 
 async fn speak(text: impl AsRef<str>) {
@@ -31,7 +31,8 @@ async fn speak(text: impl AsRef<str>) {
 #[tokio::main]
 async fn main() -> Result<(), dbus::Error> {
     //I am trying to fix this by making TTS not be lazily initialised
-    TTS.set(Mutex::new(Speaker::new("yggdrasil").unwrap())).unwrap();
+    TTS.set(Mutex::new(Speaker::new("yggdrasil").unwrap()))
+        .unwrap();
     // Connect to the accessibility bus
     let (_event_loop, conn) = open_a11y_bus().await?;
     // Create a proxy object that interacts with the at-spi registry
@@ -54,28 +55,34 @@ async fn main() -> Result<(), dbus::Error> {
     let mr = MatchRule::new_signal(StateChanged::INTERFACE, StateChanged::NAME);
     // msgmatch must be bound, else we get no events!
     let (_msgmatch, mut stream) = conn.add_match(mr).await?.msg_stream();
-        while let Some(msg) = stream.next().await {
-            let mut iter = msg.iter_init();
-            let event_type: String = iter.get().unwrap();
-            if event_type != "focused" {
-                continue;
-            }
-            iter.next(); // Done retrieving this String
-            let gained_focus = iter.get::<i32>().unwrap() == 1;
-            if !gained_focus {
-                continue;
-            }
-            
-            // Construct a proxy to the newly focused DBus object
-            // I think the only time these unwraps would panic is if we were constructing a
-            // message, and it wasn't fully constructed yet, so this *should* be fine
-            let accessible = Proxy::new(msg.sender().unwrap(), msg.path().unwrap(), TIMEOUT, Arc::clone(&conn));
-            let name_fut: MethodReply<String> = accessible.get("org.a11y.atspi.Accessible", "Name");
-            let role_fut: MethodReply<(String,)> = accessible.method_call("org.a11y.atspi.Accessible", "GetLocalizedRoleName", ());
-            let (name, (role,)) = tokio::try_join!(name_fut, role_fut)?;
-            let text = format!("{}, {}", name, role);
-            tokio::task::spawn(speak(text));
+    while let Some(msg) = stream.next().await {
+        let mut iter = msg.iter_init();
+        let event_type: String = iter.get().unwrap();
+        if event_type != "focused" {
+            continue;
         }
+        iter.next(); // Done retrieving this String
+        let gained_focus = iter.get::<i32>().unwrap() == 1;
+        if !gained_focus {
+            continue;
+        }
+
+        // Construct a proxy to the newly focused DBus object
+        // I think the only time these unwraps would panic is if we were constructing a
+        // message, and it wasn't fully constructed yet, so this *should* be fine
+        let accessible = Proxy::new(
+            msg.sender().unwrap(),
+            msg.path().unwrap(),
+            TIMEOUT,
+            Arc::clone(&conn),
+        );
+        let name_fut: MethodReply<String> = accessible.get("org.a11y.atspi.Accessible", "Name");
+        let role_fut: MethodReply<(String,)> =
+            accessible.method_call("org.a11y.atspi.Accessible", "GetLocalizedRoleName", ());
+        let (name, (role,)) = tokio::try_join!(name_fut, role_fut)?;
+        let text = format!("{}, {}", name, role);
+        tokio::task::spawn(speak(text));
+    }
     Ok(())
 }
 
