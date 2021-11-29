dd5ec9c0fce30f6beae4f71c82d1fcbfb2093fd9
Tait Hoyem <tait@tait.tech>
2021-11-28 17:24:00 -0700

    new input archtecture

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
 #[macro_use]
 extern crate lazy_static;
 use odilia_input::{
-  init as input_init,EventAction,AsyncFn
+  events::create_keybind_channel,
+  keybinds::{
+    add_keybind,
+    run_keybind_func,
+  },
 };
 use odilia_common::{
   input::{KeyBinding,KeyEvent,Modifiers},
@@ -34,24 +38,6 @@ lazy_static! {
 // TODO: static set init
   static ref ACTIVE_MODE: Arc<SyncMutex<ScreenReaderMode>> = Arc::new(SyncMutex::new(ScreenReaderMode::new("CommandMode")));
 }
-static KMAP: OnceCell<HashMap<KeyBinding, AsyncFn>> = OnceCell::new();
-
-fn find_keybind(kev: KeyEvent, mode: ScreenReaderMode) -> Option<KeyBinding> {
-    let kbdfns = KMAP.get().unwrap();
-    for (kb, _) in kbdfns.iter() {
-        let mut matches = true;
-        matches &= kev.key == kb.key;
-        matches &= (kev.mods == Modifiers::NONE && kb.mods == Modifiers::NONE) || kb.mods.intersects(kev.mods);
-        matches &= kev.repeat == kb.repeat;
-        if let Some(m1) = &kb.mode {
-            matches &= *m1 == mode;
-        }
-        if matches {
-            return Some(kb.clone());
-        }
-    }
-    None
-}
 
 async fn speak(text: impl AsRef<str>) {
     let temp = TTS.get().unwrap().lock().await;
@@ -75,52 +61,10 @@ async fn previous_header() {
   speak("Previous header").await;
 }
 
-/*
-Creating a function to turn an async function into the AsyncFn type from odilia-input
-*/
-async fn boxit<F,T>(func: F) -> AsyncFn
-where
-  F: Fn() -> T + Send + 'static + Sync,
-  T: Future<Output=()> + Send + 'static,
-{
-  Box::new(move || {Box::new(Box::pin(func()))})
-}
-
-async fn get_asyncfn_from_keybinding(kb: &KeyBinding) -> Option<&'static AsyncFn> {
-  if let Some(kmap) = KMAP.get() {
-    let func = kmap.get(kb).expect("KeyBinding asyncfn not found!");
-    return Some(func);
-  }
-  None
-}
-
-async fn get_keybinding_from_event(event: &KeyEvent) -> Option<KeyBinding> {
-  if let Ok(mode) = ACTIVE_MODE.lock() {
-    if let Some(kb) = find_keybind(event.clone(), mode.clone()) {
-        return Some(kb.clone());
-    }
-  }
-  None
-}
-
-/* this needs to run as fast as possible; the longer it takes, the longer it takes a key to propagate, if it is not consumed. */
-fn rdev_event_bs(ev: &KeyEvent) -> (bool, bool) {
-  // I don't like needing to wait on an Arc<Mutex<_>> here. There must be a better way...
-  // and the cloning... there is a better way, I just don't know how yet.
-  if let Ok(mode) = ACTIVE_MODE.lock() {
-      if let Some(kb) = find_keybind(ev.clone(), mode.clone()) {
-          return (kb.notify, kb.consume);
-        }
-      }
-  // fail safe case, just pass everything through; do not notify; do not pass go; do not collect $200
-  (false, false)
-}
-
 async fn nothing(){}
 
 #[tokio::main]
 async fn main() -> Result<(), dbus::Error> {
-    let mut kmap = HashMap::new();
     // always consume caps lock
     let ocap = KeyBinding {
         key: None,
@@ -130,24 +74,20 @@ async fn main() -> Result<(), dbus::Error> {
         mode: None,
         notify: false,
     };
-    kmap.insert(ocap, boxit(nothing).await);
-    kmap.insert("h".parse().unwrap(), boxit(next_header).await);
-    kmap.insert("C|Odilia+f".parse().unwrap(), boxit(find_in_tree).await);
-    kmap.insert("Shift+h".parse().unwrap(), boxit(previous_header).await);
-    let _res = KMAP.set(kmap);
+    add_keybind(ocap, nothing).await;
+    add_keybind("h".parse().unwrap(), next_header).await;
+    add_keybind("C|Odilia+f".parse().unwrap(), find_in_tree).await;
+    add_keybind("Shift+h".parse().unwrap(), previous_header).await;
 
     println!("STARTING ODILIA!");
     //I am trying to fix this by making TTS not be lazily initialised
     TTS.set(Mutex::new(Speaker::new("odilia").unwrap())).unwrap();
     speak_non_interrupt("welcome to odilia!").await;
 
-    let mut rx = input_init(rdev_event_bs);
-    while let Some(ev) = rx.recv().await {
-      let kb = get_keybinding_from_event(&ev).await.expect("Something is wrong with the keybinding send over channel!");
-      let func = get_asyncfn_from_keybinding(&kb).await.expect("Something is wrong!");
-      tokio::task::spawn(async move {
-        func().await;
-      });
+    let mut rx = create_keybind_channel();
+    while let Some(kb) = rx.recv().await {
+      // need to do this explicitly for now
+      run_keybind_func(&kb).await;
     }
     // get key event listeners set up
     /*
